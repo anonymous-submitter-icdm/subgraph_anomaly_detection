3861e6e4d3c5e9f53d4e96f1689a8c37961372f5
submitter <99469481+anonymous-submitter-icdm@users.noreply.github.com>
2022-06-10 16:20:58 -0400

    Add files via upload

---
+++ run_unsupervised.sh
@@ -0,0 +1,5 @@
+python3 main_unsupervised.py --data_dir ./data/real_world  \
+                          --results_dir ./results/real_world \
+                          --real_world_name cora \
+                          --num_anchors 50 \
+                          --embedding_channels 16
